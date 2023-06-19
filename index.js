14f9f7ce80c773a868e89c91fa21368a2f24ce38
Ganesh2421 <ganeshkeni24@gmail.com>
2023-06-19 08:57:27 +0530

    Lasts

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hello");
+var swiper = new Swiper(".product-slider", {
+  loop: true,
+  loopAdditionalSlides: 0,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
